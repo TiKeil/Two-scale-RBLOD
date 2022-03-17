a141de4fdc7fc223afe04b823f2eb04a047fd9b8
Tim Keil <tim.keil@wwu.de>
2022-03-17 12:43:46 +0100

    remove everything else

--- scripts/test_scripts/estimator_study_prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../..
-source venv/bin/activate
-cd scripts
-OMP_NUM_THREADS=1 mpirun -n 20 python run_experiment.py 16 256 0.001 0.000000001 1 0 --mpi --ces --p
--- scripts/test_scripts/model_problem_2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../..
-source venv/bin/activate
-cd scripts
-OMP_NUM_THREADS=1 mpirun python run_experiment.py 64 8192 0.01 0.0001 10 1 --mpi --oc --sc --sld
